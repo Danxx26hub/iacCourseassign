9d21273785101e3797f9af89f2d5f6bdedf6cc14
Danxx26hub <danxx26@gmail.com>
2021-09-28 19:23:59 +0000

    Added formatting in the load balancer section

--- main.tf
+++ main.tf
@@ -86,7 +86,6 @@ resource "azurerm_availability_set" "availSet" {
 }
 
 resource "azurerm_lb" "main-lb" {
-
   name                = "${var.prefix}-lb"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
